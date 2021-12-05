dbabfd2fe8a04b641aa16158ab6d385664ca6f7e
Guzman Valadez Jose Enrique <70705966+guveita@users.noreply.github.com>
2021-12-05 15:08:09 -0600

    CARPETA STATUS CONSOLE

---
+++ StatusConsole/StatusConsole/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace StatusConsole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxClientes = new System.Windows.Forms.ComboBox();
+            this.readStatus = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.etiquetaTempBD = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelArduino = new System.Windows.Forms.Label();
+            this.registrarUpdateTemp = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.actualizarB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clientes registrados: ";
+            // 
+            // comboBoxClientes
+            // 
+            this.comboBoxClientes.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxClientes.FormattingEnabled = true;
+            this.comboBoxClientes.Location = new System.Drawing.Point(217, 22);
+            this.comboBoxClientes.Name = "comboBoxClientes";
+            this.comboBoxClientes.Size = new System.Drawing.Size(180, 25);
+            this.comboBoxClientes.TabIndex = 1;
+            // 
+            // readStatus
+            // 
+            this.readStatus.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.readStatus.Location = new System.Drawing.Point(419, 63);
+            this.readStatus.Name = "readStatus";
+            this.readStatus.Size = new System.Drawing.Size(183, 23);
+            this.readStatus.TabIndex = 2;
+            this.readStatus.Text = "Consultar estado:";
+            this.readStatus.UseVisualStyleBackColor = true;
+            this.readStatus.Click += new System.EventHandler(this.readStatus_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Temp. registrada en BD: ";
+            // 
+            // etiquetaTempBD
+            // 
+            this.etiquetaTempBD.AutoSize = true;
+            this.etiquetaTempBD.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.etiquetaTempBD.ForeColor = System.Drawing.Color.ForestGreen;
+            this.etiquetaTempBD.Location = new System.Drawing.Point(242, 66);
+            this.etiquetaTempBD.Name = "etiquetaTempBD";
+            this.etiquetaTempBD.Size = new System.Drawing.Size(43, 20);
+            this.etiquetaTempBD.TabIndex = 4;
+            this.etiquetaTempBD.Text = "--- ---";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Temperatura actual: ";
+            // 
+            // labelArduino
+            // 
+            this.labelArduino.AutoSize = true;
+            this.labelArduino.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelArduino.ForeColor = System.Drawing.Color.Red;
+            this.labelArduino.Location = new System.Drawing.Point(242, 111);
+            this.labelArduino.Name = "labelArduino";
+            this.labelArduino.Size = new System.Drawing.Size(43, 20);
+            this.labelArduino.TabIndex = 6;
+            this.labelArduino.Text = "--- ---";
+            // 
+            // registrarUpdateTemp
+            // 
+            this.registrarUpdateTemp.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registrarUpdateTemp.Location = new System.Drawing.Point(419, 104);
+            this.registrarUpdateTemp.Name = "registrarUpdateTemp";
+            this.registrarUpdateTemp.Size = new System.Drawing.Size(183, 23);
+            this.registrarUpdateTemp.TabIndex = 7;
+            this.registrarUpdateTemp.Text = "Registrar Nueva Temp.";
+            this.registrarUpdateTemp.UseVisualStyleBackColor = true;
+            this.registrarUpdateTemp.Click += new System.EventHandler(this.registrarUpdateTemp_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label3.Location = new System.Drawing.Point(291, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Cº";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(291, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Cº";
+            // 
+            // actualizarB
+            // 
+            this.actualizarB.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.actualizarB.Location = new System.Drawing.Point(419, 24);
+            this.actualizarB.Name = "actualizarB";
+            this.actualizarB.Size = new System.Drawing.Size(183, 23);
+            this.actualizarB.TabIndex = 10;
+            this.actualizarB.Text = "Actualizar";
+            this.actualizarB.UseVisualStyleBackColor = true;
+            this.actualizarB.Click += new System.EventHandler(this.actualizarB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 154);
+            this.Controls.Add(this.actualizarB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.registrarUpdateTemp);
+            this.Controls.Add(this.labelArduino);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.etiquetaTempBD);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.readStatus);
+            this.Controls.Add(this.comboBoxClientes);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Status Console";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxClientes;
+        private System.Windows.Forms.Button readStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label etiquetaTempBD;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelArduino;
+        private System.Windows.Forms.Button registrarUpdateTemp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button actualizarB;
+    }
+}
+
---
+++ StatusConsole/StatusConsole/Form1.cs
@@ -0,0 +1,254 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace StatusConsole
+{
+    public partial class Form1 : Form
+    {
+        //Definiendo variables globales necesarias para arduino
+        public SerialPort serialPort1;
+
+        //Definiendo variables globales
+        public double temperatura;
+        public string nombre = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Método para llenar el combobox a penas inicie el programa
+            llenarCombobox();
+
+
+            //Inicializando puerto serial
+            serialPort1 = new SerialPort();
+            serialPort1.PortName = "COM4";
+            serialPort1.BaudRate = 9600;
+            serialPort1.DtrEnable = true;
+            serialPort1.Open();
+
+            serialPort1.DataReceived += serialPort1_DataReceived;
+        }
+
+        //3 métodos necesarios para la comunicación entre c# y arduino
+        public void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string line = string.Empty;
+
+
+            line = serialPort1.ReadLine();
+
+
+            this.BeginInvoke(new LineReceivedEvent(LineReceived), line);
+        }
+
+        public delegate void LineReceivedEvent(string line);
+
+        public void LineReceived(string line)
+        {
+            labelArduino.Text = line.TrimEnd('\r');
+        }
+
+
+        //método para leer la temperatura del usuario seleccionado por el combobox
+        private void readStatus_Click(object sender, EventArgs e)
+        {
+            if (comboBoxClientes.Text == "")
+            {
+                MessageBox.Show("Por favor seleccione un cliente");
+            }
+            else
+            {
+                //Llenando variable 'nombre' con el combobox seleccionado
+                nombre = comboBoxClientes.Text;
+
+                //Llenando variable 'temperatura' con la temperatura correspondiente del nombre seleciconado por el combobox
+                temperatura = obtenerTemperatura(nombre);
+
+                //Mostrando en la etiqueta 'etiquetaTempBD' la temperatura correspondiente según la BD
+                etiquetaTempBD.Text = temperatura.ToString();
+            }
+        }
+
+        //Llenando el combobox con SOLO los nombres de los clientes registrados en la base de datos
+        public void llenarCombobox()
+        {
+            comboBoxClientes.Items.Clear();
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=registros;";
+            // Tu consulta en SQL
+            string query = "SELECT * FROM clientes_tabla ORDER BY id ASC";
+
+            // Prepara la conexión
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            // A consultar !
+            try
+            {
+                // Abre la base de datos
+                databaseConnection.Open();
+
+                // Ejecuta la consultas
+                reader = commandDatabase.ExecuteReader();
+
+                // Hasta el momento todo bien, es decir datos obtenidos
+
+                // IMPORTANTE :#
+                // Si tu consulta retorna un resultado, usa el siguiente proceso para obtener datos
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // En nuestra base de datos, el array contiene:  ID 0, FIRST_NAME 1,LAST_NAME 2, ADDRESS 3
+                        // Hacer algo con cada fila obtenida
+
+                        comboBoxClientes.Items.Add(reader.GetString(1));
+                    }
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("No se encontraron datos.");
+                }
+
+                // Cerrar la conexión
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Mostrar cualquier excepción
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //Obteniendo valor de la temperatura de la BD con el nombre
+        public double obtenerTemperatura(string nombre)
+        {
+            double temp = 0;
+
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=registros;";
+            // Tu consulta en SQL
+            string query = "SELECT * FROM clientes_tabla WHERE `nombre`= '" + nombre +"'";
+
+            // Prepara la conexión
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            // A consultar !
+            try
+            {
+                // Abre la base de datos
+                databaseConnection.Open();
+
+                // Ejecuta la consultas
+                reader = commandDatabase.ExecuteReader();
+
+                // Hasta el momento todo bien, es decir datos obtenidos
+
+                // IMPORTANTE :#
+                // Si tu consulta retorna un resultado, usa el siguiente proceso para obtener datos
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // En nuestra base de datos, el array contiene:  ID 0, FIRST_NAME 1,LAST_NAME 2, ADDRESS 3
+                        // Hacer algo con cada fila obtenida
+                        if (Convert.ToDouble(reader.GetString(7)) == 0)
+                        {
+                            MessageBox.Show("Temperatura aún no registrada para " + reader.GetString(1) + "");
+                            
+                            temp = Convert.ToDouble(reader.GetString(7));
+                        }
+                        else
+                        {
+                            MessageBox.Show("¡Temperatura encontrada para: " + reader.GetString(1) + "!");
+                            temp = Convert.ToDouble(reader.GetString(7));
+                        }
+                    }
+                    Console.ReadKey();
+                }
+                else
+                {
+                    MessageBox.Show("No se encontraron datos.");
+                    temp = 0;
+                }
+
+                // Cerrar la conexión
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Mostrar cualquier excepción
+                Console.WriteLine(ex.Message);
+            }
+            return temp;
+        }
+
+        //Botón de registrar temperatura actual en la base de datos
+        private void registrarUpdateTemp_Click(object sender, EventArgs e)
+        {
+            if (comboBoxClientes.Text == string.Empty)
+            {
+                MessageBox.Show("Por favor seleccione un cliente");
+            }
+            else
+            {
+                string temp = (labelArduino.Text).ToString();
+                string temp2 = temp.TrimEnd('0');
+                string temp3 = temp2.TrimEnd('.');
+                
+                //mandando llamar método para hacer update
+                updateUser(comboBoxClientes.Text, temp3);
+            }
+        }
+
+        //método que actualiza
+        public static void updateUser(string nombre, string temperatura)
+        {
+            string connectionString = "datasource=localhost;port=3306;username=root;password=;database=registros;";
+            // Actualizar la fila user con ID 1
+            string query = "UPDATE `clientes_tabla` SET  `temperatura`='"+ temperatura + "' WHERE `nombre`='"+ nombre +"'";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+                // Actualizado satisfactoriamente
+                MessageBox.Show("Actualizado satisfactoriamente");
+                
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Ops, quizás el ID no existe
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void actualizarB_Click(object sender, EventArgs e)
+        {
+            llenarCombobox();
+        }
+    }
+}
